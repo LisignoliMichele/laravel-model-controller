4c980708cff476fcc2e4f542f8d7bc87ee872a51
Michele Lisignoli <michele@lisignoli.com>
2021-05-22 14:02:13 +0200

    form old ( values )

--- resources/views/movies_admin/create.blade.php
+++ resources/views/movies_admin/create.blade.php
@@ -21,25 +21,25 @@
       <div class="form-group">
         <label for="Titolo" class="col-sm-2 control-label">TITOLO</label>
         <div class="col-sm-10">
-          <input name="title" type="text" class="form-control" id="Titolo" placeholder="Titolo del film" value="{{old('title')}}"">
+          <input name="title" type="text" class="form-control" id="Titolo" placeholder="Titolo del film" value="{{old('title')}}">
         </div>
       </div>
       <div class="form-group">
          <label for="Regista" class="col-sm-2 control-label">REGISTA</label>
          <div class="col-sm-10">
-           <input name="movie_director" type="text" class="form-control" id="Regista" placeholder="Regista">
+           <input name="movie_director" type="text" class="form-control" id="Regista" placeholder="Regista" value="{{old('movie_director')}}">
          </div>
        </div>
        <div class="form-group">
          <label for="Genere" class="col-sm-2 control-label">GENERE</label>
          <div class="col-sm-10">
-           <input name="genres" type="text" class="form-control" id="Genere" placeholder="Genere">
+           <input name="genres" type="text" class="form-control" id="Genere" placeholder="Genere" value="{{old('genre')}}">
          </div>
        </div>
        <div class="form-group">
          <label for="Trama" class="col-sm-2 control-label">TRAMA</label>
          <div class="col-sm-10">
-            <textarea name="plot" id="Trama" cols="30" rows="10" class="form-control" placeholder="Trama"></textarea>
+            <textarea name="plot" id="Trama" cols="30" rows="10" class="form-control" placeholder="Trama">{{old('plot')}}"</textarea>
          </div>
        </div>
        <div class="form-group">
@@ -47,7 +47,7 @@
          <div class="col-sm-10">
             <select name="year" id="anno" class="form-control">
                @for ($i = 1900; $i < date("Y") + 1; $i++)
-               <option value="{{$i}}">{{$i}}</option>
+               <option value="{{$i}}" {{$i == old('year') ? 'selected' : ''}}>{{$i}}</option>
                @endfor
                
             </select>
